bc0d3c2351768ac1c26eeb39dea77431b7a7f7ab
Lee0701 <hgl1002@naver.com>
2022-05-18 15:31:30 +0900

    Add tag filtering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,49 @@
+import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Layout from '../components/layout'
-import Card from '../components/card'
 import cards from '../assets/cards.json'
+import _tagStyles from '../assets/tag-styles.json'
+import Twemoji from 'react-twemoji'
+
+const tagStyles: { [tag: string]: {bg: string, text: string} } = _tagStyles
 
 const Home: NextPage = () => {
+
+    const [visibleTags, setVisibleTags] = useState<string[]>([])
+
+    const filteredCards = cards.filter(({tags}) => visibleTags.length == 0 || tags.some((tag) => visibleTags.includes(tag)))
+
+    const onTagClick = (tag: string) => {
+        if(visibleTags.includes(tag)) visibleTags.splice(visibleTags.indexOf(tag), 1)
+        else visibleTags.push(tag)
+        setVisibleTags([...visibleTags])
+    }
+
     return (
         <Layout>
-            <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-                {cards.map((card) => {
-                    return (<Card
-                        emoji={card.emoji}
-                        title={card.title}
-                        description={card.description}
-                        href={card.href}
-                        tags={card.tags} />
+            <div className="flex-1 mt-6 flex max-w-4xl flex-wrap items-start justify-around sm:w-full">
+                {filteredCards.map(({emoji, title, description, href, tags}) => {
+                    return (
+                        <a
+                            href={href}
+                            className="group mx-2 my-6 w-96 h-64 rounded-xl p-6 text-left bg-white shadow-lg hover:bg-gray-50 a ctive:shadow-md">
+                            
+                            <Twemoji options={{ className: "text-8xl twemoji" }}>{emoji}</Twemoji>
+                            <h3 className="mt-2 text-2xl font-bold group-hover:text-orange-500 group-focus:text-orange-500">{title} &rarr;</h3>
+                            <p className="flex flex-row mt-2">
+                                {tags.map((tag) => {
+                                    const color = (tagStyles[tag] || {bg: '#e0e0e0', text: "black"})
+                                    const style = { backgroundColor: color.bg, color: color.text}
+                                    return (
+                                        <a href="#" onClick={() => onTagClick(tag)} className="mr-1 py-1 px-4 rounded-md hover:mix-blend-multiply" style={style}>{tag}</a>
+                                    )
+                                })}
+                            </p>
+                            <p className="mt-2 text-xl">{description}</p>
+                        </a>
                     )
                 })}
                 <div className="w-96 mx-2"></div>
-                {/* <Card
-                    emoji=""
-                    title=""
-                    description=""
-                    href="" /> */}
-
             </div>
         </Layout>
     )
