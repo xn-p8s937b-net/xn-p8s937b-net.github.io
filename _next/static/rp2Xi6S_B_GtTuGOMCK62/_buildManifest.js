b955093024cfb00d7d5ccfa458f1897c56ea617e
Lee0701 <hgl1002@naver.com>
2022-05-19 04:47:20 +0000

    Deploying to gh-pages from @ ko-Kore/ko-Kore.github.io@64b5fea10e14ca41c684e9a3382598a52c7c9bc4 🚀

---
+++ _next/static/rp2Xi6S_B_GtTuGOMCK62/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/162-912ae9495ae64053.js","static/chunks/pages/index-76f290b8f9dfdccb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
