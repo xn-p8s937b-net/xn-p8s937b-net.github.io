1346049ba8ca9148b6506b456fb1753228504673
Lee0701 <hgl1002@naver.com>
2024-04-05 04:17:19 +0000

    Deploying to gh-pages from @ xn-p8s937b-net/xn-p8s937b-net.github.io@4ab129b68fc83f964d5f43abe1a608f8f12533cd 🚀

---
+++ _next/static/BI_v-b9ZPvu8ZTGr0WXMq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/260-ffe43e44803728e9.js","static/chunks/pages/index-aa2c13c3feb97f26.js"],"/_error":["static/chunks/pages/_error-8fc679f694cec9ea.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/WkrRf91TP4fER08c_r_C5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/260-ffe43e44803728e9.js","static/chunks/pages/index-287f0b7887988bd7.js"],"/_error":["static/chunks/pages/_error-8fc679f694cec9ea.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
